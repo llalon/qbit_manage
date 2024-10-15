665dc63e26377a6ef4a841d76863068b5b76a26c
llalon <71791017+llalon@users.noreply.github.com>
2024-10-14 21:49:51 -0400

    Update remove_stalled.py

--- modules/core/remove_stalled.py
+++ modules/core/remove_stalled.py
@@ -13,72 +13,75 @@ class RemoveStalled:
         self.qbt = qbit_manager
         self.config = qbit_manager.config
         self.client = qbit_manager.client
-        self.stats = 0
+        self.stats = None
 
         self.db_file = os.path.join(self.config.default_dir, "stalled.db")
-        self.db_conn = None
-
         self.setup_database()
 
         self.executor = ThreadPoolExecutor(max_workers=max(os.cpu_count() - 1, 1))
-        self.collect_torrent_info()
-        self.action_stalled()
+
+        self.logger.separator("Checking Stalled Torrents", space=False, border=False)
+
+        future_collect = self.executor.submit(self.collect_torrent_info)
+        future_action = self.executor.submit(self.action_stalled)
+
+        future_collect.result()
+        future_action.result()
+
         self.executor.shutdown()
 
-        self.close_database()
+    def get_connection(self):
+        """Open a sqlite connection"""
+        return sqlite3.connect(self.db_file)
 
     def setup_database(self):
-        """Initializes SQLite database and creates table if it doesn't exist."""
+        """Create required tables for sqlite data for tracking torrent history"""
+
         try:
-            self.db_conn = sqlite3.connect(self.db_file)
             self.logger.debug(f"Connected to SQLite database at {self.db_file}")
 
             schema = """
-            CREATE TABLE IF NOT EXISTS stalled_torrents (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                torrent_id TEXT NOT NULL,
-                timestamp TEXT NOT NULL,
-                state TEXT NOT NULL
-            );
-            """
-            with self.db_conn:
-                self.db_conn.execute(schema)
+                CREATE TABLE IF NOT EXISTS stalled_torrents (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    torrent_id TEXT NOT NULL,
+                    timestamp TEXT NOT NULL,
+                    state TEXT NOT NULL
+                );
+                """
+            with self.get_connection() as db_conn:
+                db_conn.execute(schema)
         except sqlite3.Error as e:
             self.logger.error(f"SQLite connection error: {e}")
 
-    def close_database(self):
-        """Closes the SQLite database connection safely."""
-        if self.db_conn:
-            try:
-                self.db_conn.close()
-                self.logger.debug(f"Connection to SQLite database at {self.db_file} closed")
-            except sqlite3.Error as e:
-                self.logger.error(f"SQLite connection error during close: {e}")
-
     def action_stalled(self):
-        pass
+        """Action torrents based on the configuration"""
+        self.logger.debug("Processing stalled torrents...")
+
+        self.stats = 0
+
+        with self.get_connection() as db_conn:
+            db_conn.execute("SELECT * FROM stalled_torrents")
 
     def collect_torrent_info(self):
-        """Collect current torrent info with timestamp"""
+        """Collect and save torrent info with timestamps to database"""
 
-        self.logger.separator("Checking Stalled Torrents", space=False, border=False)
+        self.logger.debug("Collecting torrent info...")
 
         torrents = self.client.torrents_info()
 
         self.logger.print_line(f"Saving info for {len(torrents)} torrents", self.config.loglevel)
 
         insert_query = """
-        INSERT INTO stalled_torrents (torrent_id, timestamp, state)
-        VALUES (?, ?, ?)
-        """
+            INSERT INTO stalled_torrents (torrent_id, timestamp, state)
+            VALUES (?, ?, ?)
+            """
 
         timestamp = int(time.time())
 
-        # Batch insertion to optimize performance
-        data_to_insert = [(torrent.hash, timestamp, torrent.state) for torrent in torrents]
+        data = [(torrent.hash, timestamp, torrent.state) for torrent in torrents]
         try:
-            with self.db_conn:
-                self.db_conn.executemany(insert_query, data_to_insert)
-            self.logger.debug(f"Collected and inserted info for {len(data_to_insert)} torrents")
+            with self.get_connection() as db_conn:
+                db_conn.executemany(insert_query, data)
+            self.logger.debug(f"Collected and inserted info for {len(data)} torrents")
         except sqlite3.Error as e:
             self.logger.error(f"SQLite insertion error: {e}")
